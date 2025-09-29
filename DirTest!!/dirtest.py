709ac951d75e5fc6e95dad575532a0c8451d859a
NullDruid <bprabhanjen@gmail.com>
2025-09-29 10:09:00 -0500

    Update dirtest.py

--- DirTest!!/dirtest.py
+++ DirTest!!/dirtest.py
@@ -1,20 +1,25 @@
 import requests
 import time
-l=[]
-print("----- Welcome to DirTest! ----- \n")
-time.sleep(3)
-target = input("\nEnter target url :")
-file = input("Enter the payload file location :")
-wordlist = open(file,"r")
-for loop in wordlist:
-    response = requests.get(url=f"{target}/{loop}")
-    code=str(response)[11:14]
-    if int(code[0]) == 1 or int(code[0]) == 2 or int(code[0]) == 3:
-      print(f"{target}/{loop}{code}")
-      l.append(loop[0:-2])
-    else:
-        print(f"{target}/{loop} encounterd a error {code}")
-time.sleep(5)
-print("Available directory from the list are :",l)
-time.sleep(5)
+s=True
+while(s):
+  l=[]
+  print("----- Welcome to DirTest! ----- \n")
+  time.sleep(0.5)
+  target = input("\nEnter target url :")
+  file = input("Enter the payload file location :")
+  wordlist = open(file,"r")
+  for loop in wordlist:
+      response = requests.get(url=f"{target}/{loop.strip()}")
+      code=str(response.status_code)
+      if int(code[0]) == 1 or int(code[0]) == 2 or int(code[0]) == 3:
+        print(f"{target}/{loop} does exists! {code}")
+        l.append(loop[0:-2])
+      else:
+          print(f"{target}/{loop} hit a wall {code} ")
+  time.sleep(0.9)
+  print("Available directory from the list are : ",l)
+  time.sleep(3)
+  choice = input("Do you wanna exit (Y/N)")
+  if choice.lower() == 'y':
+    s = False 
 print("--------- END ---------")
